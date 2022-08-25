4d8114559fa940e472df04530d63cd695fbc709a
IDRISSHACKER <idrisscoder@gmail.com>
2022-08-25 15:12:07 +0100

    Add support for external link

--- src/screens/Download/Download.tsx
+++ src/screens/Download/Download.tsx
@@ -1,22 +1,31 @@
-import { Box, Typography } from "@mui/material"
-import React from "react"
-import HeadSection from './HeadSection/HeadSection';
+// @ts-nocheck
+import { Box, Typography } from "@mui/material";
+import React from "react";
+import HeadSection from "./HeadSection/HeadSection";
 import DownloadBody from "./DownloadBody/DownloadBody";
 import Page from "../../components/Page";
+import { useDispatch, useSelector } from "react-redux";
+import { getSoftware } from "./../../redux/slices/softwareSlice/softwareSlice";
 
-function Download(){
+function Download() {
+  const dispatch = useDispatch();
+  const loading = useSelector((state) => state.software.loading);
 
-    return (
-      <Page title={"Téléchargements"}>
-        <div className="download">
-          <div>
-            <HeadSection />
-            <DownloadBody />
-            <Box sx={{ height: 200 }} />
-          </div>
+  React.useEffect(() => {
+    dispatch(getSoftware());
+  }, [dispatch]);
+
+  return (
+    <Page title={"Téléchargements"}>
+      <div className="download">
+        <div>
+          <HeadSection />
+          <DownloadBody />
+          <Box sx={{ height: 200 }} />
         </div>
-      </Page>
-    );
+      </div>
+    </Page>
+  );
 }
 
-export default Download
+export default Download;
--- src/screens/Download/DownloadBody/DownloadBody.tsx
+++ src/screens/Download/DownloadBody/DownloadBody.tsx
@@ -1,117 +1,194 @@
-import React  from "react"
-import { Link } from "react-router-dom"
-import { StoreContainer } from "../../../components/container/container"
+// @ts-nocheck
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
+import { StoreContainer } from "../../../components/container/container";
 import PhoneAndroidOutlinedIcon from "@mui/icons-material/PhoneAndroidOutlined";
 import PersonalVideoOutlinedIcon from "@mui/icons-material/PersonalVideoOutlined";
 import LaunchOutlinedIcon from "@mui/icons-material/LaunchOutlined";
-import "./DownloadBody.scss"
-import { AndroidOutlined } from '@mui/icons-material';
-import AppleIcon from '@mui/icons-material/Apple';
-import appleSrc from "./AppleStore.png"
-import androidSrc from "./playStore.png"
+import "./DownloadBody.scss";
+import { AndroidOutlined } from "@mui/icons-material";
+import AppleIcon from "@mui/icons-material/Apple";
+import appleSrc from "./AppleStore.png";
+import androidSrc from "./playStore.png";
 import CinafBtn from "../../../components/cinafBtn/CinafBtn";
+import { getSoftware } from "./../../../redux/slices/softwareSlice/softwareSlice";
+import { json } from "stream/consumers";
 
-function DownloadBody(){
-
-
-    return (
-      <div className="mb-10">
-        <div className="downloadBody">
-          <StoreContainer>
-            <div className="downloadBody-content">
-              <div className="d-head">
-                <DHeadBody />
-              </div>
+function DownloadBody() {
+  return (
+    <div className="mb-10">
+      <div className="downloadBody">
+        <StoreContainer>
+          <div className="downloadBody-content">
+            <div className="d-head">
+              <DHeadBody />
+            </div>
+            <br />
+            <div className="d-body">
+              <DTitle icon={<PhoneAndroidOutlinedIcon />}>
+                Smartphones et tablettes
+              </DTitle>
+              <DDownloadContent />
               <br />
-              <div className="d-body">
-                <DTitle icon={<PhoneAndroidOutlinedIcon />}>
-                  Smartphones et tablettes
-                </DTitle>
-                <DDownloadContent />
-                <br /><br />
-                <DTitle icon={<PersonalVideoOutlinedIcon />}>
-                  Smart TV
-                </DTitle>
-                <DDownloadContent />
-              </div>
+              <br />
+              <DTitle icon={<PersonalVideoOutlinedIcon />}>Smart TV</DTitle>
+              <DDownloadContentTV />
             </div>
-          </StoreContainer>
-        </div>
-      </div>
-    );
-}
-
-function DHeadBody(){
-
-    return (
-      <div className="d-head-body">
-        <h2 className="h2 white bTitle">Toutes les applications CINAF</h2>
-        <p className="whiteL">
-          Nous avons des applications pour toutes les plateformes et appareils
-          populaires. Installez tout simplement Cinaf sur votre écran préféré,
-          connectez-vous à votre compte et lancez-vous !
-        </p>
-      </div>
-    );
-}
-
-
-function DTitle({children, icon}:any){
-    return (
-      <div className="bloc-title">
-        <div className="bloc-title-icon">{icon}</div>
-        <div className="bloc-title-text">
-          <h3>{children}</h3>
-        </div>
-      </div>
-    );
-}
-
-function DDownloadContent(){
-    return (
-        <div className="DDownload">
-            <div className="DDownload-body">
-                <DDownloadItem to="/download/cinaf-ios" icon={<AndroidOutlined />} img={androidSrc}>Cinaf pour android</DDownloadItem>
-                <DDownloadItem to="/download/cinaf-android" icon={<AppleIcon />} img={appleSrc}>Cinaf pour ios</DDownloadItem>
-            </div>
-        </div>
-    )
-}
-
-function DDownloadItem({children, icon, to="", isLink=true, img=androidSrc}:any){
-    return (
-      <div className="DDownloadItem">
-        <div className="DIcon">{icon}</div>
-        <div className="DText">{children}</div>
-        {isLink && (
-          <div className="DLink">
-            <DDownloadLink to={to} icon={<img src={img} alt="" />}>Telecharger sur le store</DDownloadLink>
-            <MagnetDownloadLink to={to}>Telecharger</MagnetDownloadLink>
           </div>
-        )}
+        </StoreContainer>
       </div>
-    );
+    </div>
+  );
 }
 
-export function DDownloadLink({to, children, icon}:any){
-    return (
-      <Link to={to} className="downloadLink">
-        <div className="dlIcon">{icon}</div>
-        <div className="dlText">{children}</div>
-        <div>
-            <LaunchOutlinedIcon />
+function DHeadBody() {
+  return (
+    <div className="d-head-body">
+      <h2 className="h2 white bTitle">Toutes les applications CINAF</h2>
+      <p className="whiteL">
+        Nous avons des applications pour toutes les plateformes et appareils
+        populaires. Installez tout simplement Cinaf sur votre écran préféré,
+        connectez-vous à votre compte et lancez-vous !
+      </p>
+    </div>
+  );
+}
+
+function DTitle({ children, icon }: any) {
+  return (
+    <div className="bloc-title">
+      <div className="bloc-title-icon">{icon}</div>
+      <div className="bloc-title-text">
+        <h3>{children}</h3>
+      </div>
+    </div>
+  );
+}
+
+function DDownloadContent() {
+  
+  const softwares = useSelector((state: any) => state.software);
+
+  return (
+    <div className="DDownload">
+      <div className="DDownload-body">
+        <DDownloadItem
+          toStore={`${
+            !softwares.loading &&
+            !softwares.error &&
+            softwares?.software[2]?.store
+          }`}
+          to={`${
+            !softwares.loading &&
+            !softwares.error &&
+            softwares?.software[2]?.slug
+          }`}
+          icon={<AndroidOutlined />}
+          img={androidSrc}
+        >
+          {!softwares.loading &&
+            !softwares.error &&
+            softwares?.software[2]?.title}
+        </DDownloadItem>
+        <DDownloadItem
+          toStore={`${
+            !softwares.loading &&
+            !softwares.error &&
+            softwares?.software[0]?.store
+          }`}
+          to={`/download/${
+            !softwares.loading &&
+            !softwares.error &&
+            softwares?.software[0]?.slug
+          }`}
+          icon={<AppleIcon />}
+          img={appleSrc}
+        >
+          {!softwares.loading &&
+            !softwares.error &&
+            softwares?.software[0]?.title}
+        </DDownloadItem>
+      </div>
+    </div>
+  );
+}
+
+function DDownloadContentTV() {
+
+  const softwares = useSelector((state: any) => state.software);
+
+  return (
+    <div className="DDownload">
+      <div className="DDownload-body">
+        <DDownloadItem
+          to={`/download/${!softwares.loading &&
+            !softwares.error &&
+            softwares?.software[1]?.slug}`}
+          icon={<AndroidOutlined />}
+          img={androidSrc}
+          inStore={false}
+          variant={"fw"}
+        >
+          {!softwares.loading &&
+            !softwares.error &&
+            softwares?.software[1]?.title}
+        </DDownloadItem>
+      </div>
+    </div>
+  );
+}
+
+function DDownloadItem({
+  children,
+  icon,
+  to = "",
+  toStore="",
+  isLink = true,
+  img = androidSrc,
+  inStore = true,
+}: any) {
+  return (
+    <div className="DDownloadItem">
+      <div className="DIcon">{icon}</div>
+      <div className="DText">{children}</div>
+      {isLink && (
+        <div className="DLink">
+          {inStore && (
+            <DDownloadLink to={toStore} icon={<img src={img} alt="" />}>
+              Telecharger sur le store
+            </DDownloadLink>
+          )}
+          <MagnetDownloadLink to={to}>Telecharger</MagnetDownloadLink>
         </div>
-      </Link>
-    );
+      )}
+    </div>
+  );
+}
+
+export function DDownloadLink({ to, children, icon }: any) {
+  return (
+    <a href={to} className="downloadLink" target={"_blank"} rel="noreferrer">
+      <div className="dlIcon">{icon}</div>
+      <div className="dlText">{children}</div>
+      <div>
+        <LaunchOutlinedIcon />
+      </div>
+    </a>
+  );
 }
 
 export function MagnetDownloadLink({ to, children, variant, props }: any) {
   return (
-    <CinafBtn variant={`dl applyH ${variant}`} downloadBtn to={to} props={props}>
+    <CinafBtn
+      variant={`dl applyH ${variant}`}
+      downloadBtn
+      to={to}
+      props={props}
+    >
       Telecharger
     </CinafBtn>
   );
 }
 
-
-export default DownloadBody
+export default DownloadBody;
